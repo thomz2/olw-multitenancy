67367d0d6fbfd2a20abfbe1ef8b9245972d0e7a8
Thomaz <thomaz2pinheiro@gmail.com>
2024-03-24 17:01:05 -0300

    important: Seller Livewire Component

---
+++ app/Livewire/Seller/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Seller;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.seller.edit');
+    }
+}
