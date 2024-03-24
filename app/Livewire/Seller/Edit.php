6bb0766defacad79de39e3779efadd5403d7938a
Thomaz <thomaz2pinheiro@gmail.com>
2024-03-24 19:22:03 -0300

    Seller create/update

--- app/Livewire/Seller/Edit.php
+++ app/Livewire/Seller/Edit.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Seller;
 
+use App\Livewire\Forms\SellerForm;
+use App\Models\Seller;
 use Livewire\Component;
 
 class Edit extends Component
 {
+
+    public SellerForm $form;
+
+    public function mount(Seller $seller) 
+    {
+        $this->form->setSeller($seller);
+    }
+
     public function render()
     {
         return view('livewire.seller.edit');
+    }
+
+    public function save()
+    {
+        $this->form->save();
     }
 }
